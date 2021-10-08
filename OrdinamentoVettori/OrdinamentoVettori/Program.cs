11719c9a1af3e634376fc1a98059156de79ac748
Giuseppe Fusco <GIUSEPPE.FUSCO@local.ittcesena.it>
2021-10-08 08:35:50 +0200

    aggiornamento commenti

--- OrdinamentoVettori/OrdinamentoVettori/Program.cs
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -6,19 +6,23 @@ namespace OrdinamentoVettori
     {
         static void Main(string[] args)
         {
+            //creazione e inizializzazione di un vettore di interi
             int [] vettore = new int [10] { 11, 55, -9, 7, 74, 147, -87, 6, 0, 18};
 
-
+            //richiamo metodo per l'ordinamento
             ordina(vettore);
 
+            //stampo a video il contenuto del vettore
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(vettore[i]);
-
+            
+            //evito che l'interfaccia console si chiuda
             Console.ReadKey();
         }
 
         static void ordina (int[] vettore)
         {
+            //.Sort: metodo di riordinamento degli array
             Array.Sort(vettore);
         }
 
